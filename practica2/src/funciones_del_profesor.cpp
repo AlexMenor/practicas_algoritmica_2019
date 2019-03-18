4eba2e7dc03043a96a216d0a832d519cec9ba1b6
Alex <alex4menor@gmail.com>
2019-03-18 16:58:55 +0100

    Añadido generador de tiempos con los dos algoritmos, su makefile y el guion de la practica

---
+++ practica2/src/funciones_del_profesor.cpp
@@ -0,0 +1,297 @@
+
+/**********************************************************************/
+#ifndef FUNCIONES_DEL_PROFESOR
+#define FUNCIONES_DEL_PROFESOR
+
+#include <cstdlib>
+#include <climits>
+#include <cassert>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+static int const MAX_LONG  = 10;
+static int const UMBRAL_MS  = 100;
+static int const UMBRAL_QS  = 50;
+static int const VECES = 5;
+
+/**********************************************************************/
+
+void burbuja_lims(int T[], int inicial, int final)
+{
+  int i, j;
+  int aux;
+  for (i = inicial; i < final - 1; i++)
+    for (j = final - 1; j > i; j--)
+      if (T[j] < T[j-1])
+	{
+	  aux = T[j];
+	  T[j] = T[j-1];
+	  T[j-1] = aux;
+	}
+}
+
+void burbuja (int T[], int nelem){
+  burbuja_lims(T, 0, nelem);
+}
+
+/**********************************************************************/
+
+int fibo(int n)
+{
+  if (n < 2)
+    return 1;
+  else
+    return fibo(n-1) + fibo(n-2);
+}
+
+/**********************************************************************/
+
+int ** ReservaMatriz(int dim)
+{
+  int **M;
+  if (dim  <= 0)
+    {
+      cerr<< "\n ERROR: Dimension de la matriz debe ser mayor que 0" << endl;
+      exit(1);
+    }
+  M = new int * [dim];
+  if (M == NULL)
+    {
+      cerr << "\n ERROR: No puedo reservar memoria para un matriz de "
+	   << dim << " x " << dim << "elementos" << endl;
+      exit(1);
+    }
+  for (int i = 0; i < dim; i++)
+    {
+      M[i]= new int [dim];
+      if (M[i] == NULL)
+	{
+	  cerr << "ERROR: No puedo reservar memoria para un matriz de "
+	       << dim << " x " << dim << endl;
+	  for (int j = 0; j < i; j++)
+	    delete [] M[i];
+	  delete [] M;
+	  exit(1);
+	} 
+    }
+  return M;
+}	
+
+/**********************************************************************/
+
+void LiberaMatriz(int ** & M, int dim)
+{
+  for (int i = 0; i < dim; i++)
+    delete [] M[i];
+  delete [] M;
+  M = NULL;
+}		
+
+/**********************************************************************/
+
+void RellenaMatriz(int **M, int dim)
+{
+  for (int i = 0; i < dim; i++)
+    for (int j = 0; j < dim; j++)
+      if (i != j)
+	M[i][j]= (rand() % MAX_LONG);
+}			
+
+/**********************************************************************/	
+
+void Floyd(int **M, int dim)
+{
+	for (int k = 0; k < dim; k++)
+	  for (int i = 0; i < dim;i++)
+	    for (int j = 0; j < dim;j++)
+	      {
+		int sum = M[i][k] + M[k][j];    	
+	    	M[i][j] = (M[i][j] > sum) ? sum : M[i][j];
+	      }
+}	     	
+
+/**********************************************************************/	
+
+void reajustar(int T[], int num_elem, int k)
+{
+  int j;
+  int v;
+  v = T[k];
+  bool esAPO = false;
+  while ((k < num_elem/2) && !esAPO)
+    {
+      j = k + k + 1;
+      if ((j < (num_elem - 1)) && (T[j] < T[j+1]))
+	j++;
+      if (v >= T[j])
+	esAPO = true;
+      T[k] = T[j];
+      k = j;
+    }
+  T[k] = v;
+}
+
+/**********************************************************************/	
+
+void heapsort(int T[], int num_elem)
+{
+  int i;
+  for (i = num_elem/2; i >= 0; i--)
+    reajustar(T, num_elem, i);
+  for (i = num_elem - 1; i >= 1; i--)
+    {
+      int aux = T[0];
+      T[0] = T[i];
+      T[i] = aux;
+      reajustar(T, i, 0);
+    }
+}
+  
+/**********************************************************************/	
+
+void insercion_lims(int T[], int inicial, int final)
+{
+  int i, j;
+  int aux;
+  for (i = inicial + 1; i < final; i++) {
+    j = i;
+    while ((T[j] < T[j-1]) && (j > 0)) {
+      aux = T[j];
+      T[j] = T[j-1];
+      T[j-1] = aux;
+      j--;
+    };
+  };
+}
+
+void insercion (int T[], int nelem){
+  insercion_lims (T, 0, nelem);
+}
+/**********************************************************************/	
+
+void fusion(int T[], int inicial, int final, int U[], int V[])
+{
+  int j = 0;
+  int k = 0;
+  for (int i = inicial; i < final; i++)
+    {
+      if (U[j] < V[k]) {
+	T[i] = U[j];
+	j++;
+      } else{
+	T[i] = V[k];
+	k++;
+      };
+    };
+}
+
+/**********************************************************************/	
+
+void mergesort_lims(int T[], int inicial, int final)
+{
+  if (final - inicial < UMBRAL_MS)
+    {
+      insercion_lims(T, inicial, final);
+    } else {
+      int k = (final - inicial)/2;
+
+      int * U = new int [k - inicial + 1];
+      assert(U);
+      int l, l2;
+      for (l = 0, l2 = inicial; l < k; l++, l2++)
+	U[l] = T[l2];
+      U[l] = INT_MAX;
+
+      int * V = new int [final - k + 1];
+      assert(V);
+      for (l = 0, l2 = k; l < final - k; l++, l2++)
+	V[l] = T[l2];
+      V[l] = INT_MAX;
+
+      mergesort_lims(U, 0, k);
+      mergesort_lims(V, 0, final - k);
+      fusion(T, inicial, final, U, V);
+      delete [] U;
+      delete [] V;
+    };
+}
+
+void mergesort (int T[], int nelem){
+  mergesort_lims (T, 0, nelem);
+}
+
+/**********************************************************************/	
+
+void dividir_qs(int T[], int inicial, int final, int & pp)
+{
+  int pivote, aux;
+  int k, l;
+
+  pivote = T[inicial];
+  k = inicial;
+  l = final;
+  do {
+    k++;
+  } while ((T[k] <= pivote) && (k < final-1));
+  do {
+    l--;
+  } while (T[l] > pivote);
+  while (k < l) {
+    aux = T[k];
+    T[k] = T[l];
+    T[l] = aux;
+    do k++; while (T[k] <= pivote);
+    do l--; while (T[l] > pivote);
+  };
+  aux = T[inicial];
+  T[inicial] = T[l];
+  T[l] = aux;
+  pp = l;
+};
+
+/**********************************************************************/	
+
+void quicksort_lims(int T[], int inicial, int final)
+{
+  int k;
+  if (final - inicial < UMBRAL_QS) {
+    insercion_lims(T, inicial, final);
+  } else {
+    dividir_qs(T, inicial, final, k);
+    quicksort_lims(T, inicial, k);
+    quicksort_lims(T, k + 1, final);
+  };
+}
+
+void quicksort (int T[], int nelem){
+  quicksort_lims(T, 0, nelem);
+}
+
+/**********************************************************************/	
+
+void seleccion_lims(int T[], int inicial, int final)
+{
+  int i, j, indice_menor;
+  int menor, aux;
+  for (i = inicial; i < final - 1; i++) {
+    indice_menor = i;
+    menor = T[i];
+    for (j = i; j < final; j++)
+      if (T[j] < menor) {
+	indice_menor = j;
+	menor = T[j];
+      }
+    aux = T[i];
+    T[i] = T[indice_menor];
+    T[indice_menor] = aux;
+  }
+}
+
+void seleccion (int T[], int nelem){
+  seleccion_lims(T, 0, nelem);
+}
+
+/**********************************************************************/	
+#endif
---
+++ practica2/src/genera_tiempos.cpp
@@ -0,0 +1,107 @@
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <utility>
+#include "funciones_del_profesor.cpp"
+using namespace std::chrono;
+using namespace std;
+
+pair <int, int> sumaObvia(int * v, int n, int x){
+    for (int i = 0 ; i < n-1 ; i++)
+        for (int j = i+1 ; j < n ; j++){
+            if (v[i] + v[j] == x){
+                return pair<int,int>(v[i], v[j]);
+            }
+        }
+    return pair<int,int> (0,0);
+}
+
+pair<int,int> sumaDivideYVenceras(int * v, int izda, int dcha, int x){
+    int sum = v[izda] + v[dcha];
+    if (abs(izda - dcha) == 1 || sum == x)
+        return pair <int,int> (v[izda], v[dcha]);
+    else if (sum > x)
+        return sumaDivideYVenceras(v, izda, dcha - 1, x);
+    else
+        return sumaDivideYVenceras(v, izda+1, dcha, x);
+
+}
+
+double uniforme() //Genera un n�mero uniformemente distribuido en el
+                  //intervalo [0,1) a partir de uno de los generadores
+                  //disponibles en C.
+{
+ int t = rand();
+ double f = ((double)RAND_MAX+1.0);
+ return (double)t/f;
+}
+
+int generador(int * v, int n){
+
+    for (int j=0; j<n; j++) {
+        double u=uniforme();
+        int k=(int)(2*n*u);
+        v[j]=k;
+    }
+
+    double u=uniforme();
+    return (int)(3*n*u);
+}
+
+
+int main (int argc, char ** argv){
+    const int VECES = 100;
+    int opcion, desde, hasta, incremento;
+    ofstream fichero;
+
+    if (argc == 2)
+        fichero.open(argv[1]);
+
+    ostream & salida = fichero.is_open() ? fichero : cout;
+    
+    cout << "Selecciona el algoritmo:" << endl
+    << "1) Obvio" << endl
+    << "2) Divide y vencerás" << endl;
+
+    cin >> opcion;
+    while (!(opcion > 0 && opcion < 3))
+        cin >> opcion;
+    
+    cout << "¿Desde que tamaño?" << endl;
+    cin >> desde;
+    cout << "¿Hasta que tamaño?" << endl;
+    cin >> hasta;
+    cout << "¿Qué incremento?" << endl;
+    cin >> incremento;
+    cout << "Empezamos: \n" << endl;
+
+    high_resolution_clock::time_point tantes, tdespues;
+    duration <double> transcurrido;
+
+    for (int tam = desde ; tam <= hasta ; tam += incremento){
+        double sum = 0.0;
+        int * v = new int [tam];
+        for (int i = 0 ; i < VECES ; i++){
+            int x = generador(v, tam);
+            tantes = high_resolution_clock::now();
+            pair<int,int> sol;
+            //if (opcion == 1)
+                //sol = sumaObvia(v, tam, x);
+            //else{
+                quicksort(v, tam);
+                sol = sumaDivideYVenceras(v, 0, tam - 1, x);
+            //}
+            tdespues = high_resolution_clock::now();
+
+            transcurrido = duration_cast<duration<double>> (tdespues - tantes);
+            sum += transcurrido.count();
+             // borrar despues
+             for (int k = 0 ; k < tam ; k++)
+                salida << v[k] << " ";
+            salida << endl << sol.first << " " << sol.second << " " << x << endl;  
+        }
+        salida << tam << " " << sum/VECES << endl;
+        delete [] v;
+    }
+
+}
