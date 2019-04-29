6f37f793d0dc46c2b026bed2733a04d5db2265d4
Alex <alex4menor@gmail.com>
2019-04-29 20:36:24 +0200

    inicio de la segunda parte de la práctica 3, main básico y generador de distancias implementados

---
+++ practica3/Segunda parte/viajanteGreedy.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <cmath>
+
+using namespace std;
+
+void calcularDistancias (int ** m, vector<pair<double, double>> & ciudades){
+  for (int i = 0 ; i < ciudades.size() ; i++){
+    int xi = ciudades[i].first;
+    int yi = ciudades[i].second;
+    for (int j = i+1 ; j < ciudades.size() ; j++){
+      int xj = ciudades[j].first;
+      int yj = ciudades[j].second;
+      double distancia = sqrt(pow(xi-xj,2) + pow(yi-yj,2));
+      m[i+1][j+1] = (int) ceil(distancia);
+      m[j+1][i+1] = (int) ceil(distancia);
+    }
+  }
+}
+
+// El programa tiene como argumento el fichero
+// con el "mapa", después se podrá seleccionar
+// una de las alternativas, se generará en el
+// mismo directorio un archivo salida 
+
+int main (int argc, char ** argv){
+
+  if (argc < 2){
+    cerr << "Uso: " << argv[0] << " ficheroDeEntrada.tsp" << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  ifstream entrada (argv[1]);
+
+  if (!entrada){
+    cerr << argv[1] << " no existe o es inválido" << endl;
+    exit (EXIT_FAILURE);
+  }
+
+  entrada.ignore(256, ' ');
+
+  int n;
+  entrada >> n;
+
+  vector<pair<double, double>> ciudades;
+
+  double ign, x, y;
+  for (int i = 0 ; i < n ; i++){
+    entrada >> ign >> x >> y;
+    ciudades.push_back(pair<double, double>(x,y));
+  }
+
+  int ** distancias = new int * [n+1];
+  for (int i = 0 ; i <= n ; i++)
+    distancias[i] = new int[n+1];
+
+  calcularDistancias(distancias, ciudades);
+
+  for (int i = 1 ; i <= n ; i++){
+    for (int j = 1 ; j <= n ; j++)
+      cout << distancias[i][j] << " ";
+    cout << endl;
+  }
+}
